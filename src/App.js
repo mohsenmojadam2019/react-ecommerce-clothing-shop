30bd45f5a884263e14ca4a6649c1274cc2a7a9c5
m.mojadam <mohsenmojadam2019@gmail.com>
2023-08-26 18:03:08 +0330

    create components and modules

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './modules/Home';
+import Product from './modules/Product';
+import Products from './modules/Products';
+import CategoryProducts from './modules/CategoryProducts';
+import Cart from './modules/Cart';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products/:id' element={<Product />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/categories/:name' element={<CategoryProducts />} />
+        <Route path='/cart' element={<Cart />} />
+
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
